75bee4a335ac96733b684a287bb895eefec63ad5
davidbjjj <pondavid103@gmail.com>
2025-04-04 09:49:33 -0300

    inital commit

---
+++ src/test/kotlin/com/NotasBack/NotasFacil/NotasFacilApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.NotasBack.NotasFacil
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotasFacilApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

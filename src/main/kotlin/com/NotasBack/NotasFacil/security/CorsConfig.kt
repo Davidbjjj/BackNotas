ea97d73e74b02f441bf600916441106592174d72
davidbjjj <pondavid103@gmail.com>
2025-05-18 04:50:00 -0300

    CORS sem barramento

---
+++ src/main/kotlin/com/NotasBack/NotasFacil/security/CorsConfig.kt
@@ -0,0 +1,28 @@
+package com.NotasBack.NotasFacil.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class CorsConfig {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:8081") // ou "*"
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+
+            }
+        }
+    }
+
+}
